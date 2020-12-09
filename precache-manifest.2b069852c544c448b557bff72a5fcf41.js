03d24acba2009d232aa07e9c6e6029787fa8996e
ludkg@khu.ac.kr <ludkg@khu.ac.kr>
2020-12-09 20:49:36 +0900

    Updates

---
+++ precache-manifest.2b069852c544c448b557bff72a5fcf41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6221ad71a9c89a90b66633e64e58d0a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a02b71c29c30b682026a",
+    "url": "/static/css/main.fd40bbe3.chunk.css"
+  },
+  {
+    "revision": "ee8107a4bda91cd1324f",
+    "url": "/static/js/2.31d092bb.chunk.js"
+  },
+  {
+    "revision": "6d80cc6046a43fcfb72f5495bf8999c0",
+    "url": "/static/js/2.31d092bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02b71c29c30b682026a",
+    "url": "/static/js/main.0631f666.chunk.js"
+  },
+  {
+    "revision": "f13e20d1fcd9abf43e65",
+    "url": "/static/js/runtime-main.bd2a1205.js"
+  },
+  {
+    "revision": "f6a9f98fe526d844b2d28b78eac19416",
+    "url": "/static/media/angry.f6a9f98f.png"
+  },
+  {
+    "revision": "52de6dd54c07cd2edbd0a65155b588ab",
+    "url": "/static/media/home.52de6dd5.png"
+  },
+  {
+    "revision": "dd9e8fe45a9ac5a66e8f5a43269093da",
+    "url": "/static/media/mask.dd9e8fe4.png"
+  }
+]);
